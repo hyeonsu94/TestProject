b43606d9fa1cda2f0dfcec298de733647ecf1751
hyeonsu94 <ddalsus@naver.com>
2018-03-15 14:43:50 +0900

    add switch at module

--- module.cpp
+++ module.cpp
@@ -1,11 +1,15 @@
 #include<module.hpp>
 
 Cal_module::Cal_moudle(int num1,int num2,char operating){
+    
     this->num1 = num1;
     this->num2 = num2;
     this->operating = operating;
 }
 double Cal_module::Get_result(){
 
+    switch(operating){
+
+    }
 }
 
